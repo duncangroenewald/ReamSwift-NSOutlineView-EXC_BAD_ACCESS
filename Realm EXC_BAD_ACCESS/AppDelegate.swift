44bd371830687e912aaf99f69ae569565184125a
Gianni Chiappetta <gianni@runlevel6.org>
2016-02-22 12:06:36 -0500

    Initial Commit

---
+++ Realm EXC_BAD_ACCESS/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Realm EXC_BAD_ACCESS
+//
+//  Created by Gianni Chiappetta on 2016-02-22.
+//  Copyright © 2016 Demo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
